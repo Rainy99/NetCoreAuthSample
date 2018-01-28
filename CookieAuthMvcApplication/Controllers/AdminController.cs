a29d4a47041bd78e5e9244b618a4dbb9cc8edaf2
devzhou <zhouyu@devzhou.cn>
2018-01-28 14:45:58 +0800

    add admin page

---
+++ CookieAuthMvcApplication/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CookieAuthMvcApplication.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+    }
+}
